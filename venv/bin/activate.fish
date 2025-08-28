aea7d17bdb3d5ef1599520b5843812d8a38077d2
profe-ssor <kyerematengcollins93@gmail.com>
2025-08-28 23:49:34 +0000

    Final clean requirements.txt from working virtual environment

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/professor/Desktop/nssApi/venv
+set -gx VIRTUAL_ENV /home/professor/Desktop/Nasco-Project/nssApi/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
